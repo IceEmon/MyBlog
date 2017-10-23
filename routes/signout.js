c5fbd18d78d49f4b884861dfbe7168a78a6f471c
IceEmon <chenbinsha0215@126.com>
2017-10-23 16:27:12 +0800

    Initial commit

---
+++ routes/signout.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Emonice on 2017/10/16.
+ */
+var express = require('express');
+var router = express.Router();
+
+var checkLogin = require('../middlewares/check').checkLogin;
+
+// GET /signout 登出
+router.get('/', checkLogin, function(req, res, next) {
+    // res.send(req.flash());
+    req.session.user = null;
+    req.flash('success','登出成功');
+    //登出成功后跳回主页
+    res.redirect('/posts');
+});
+
+module.exports = router;
